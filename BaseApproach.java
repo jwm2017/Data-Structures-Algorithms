a87ff5470ed83605d05cf1f45d86d764b7b9d97d
Joe <joseph.mcadams@mu.edu>
2016-03-06 18:07:24 -0600

    Foundational logic set

--- BaseApproach.java
+++ BaseApproach.java
@@ -7,16 +7,18 @@
 * with love.
 */
 
+import java.util.HashMap;
+
 public abstract class BaseApproach {
 
     public int Mode;
     public int FreqOfMode;
-    private int[] List;
+    public int[] List;
 
     public BaseApproach(int[] inputList) {
         this.List = inputList;
-        this.Mode = 0;
-        this.FreqOfMode = 0;
+        this.Mode = 0;          // default value
+        this.FreqOfMode = 0;    // default value
     }
 
     public abstract void FindMode();
@@ -24,8 +26,11 @@ public abstract class BaseApproach {
 
 class BruteForce extends BaseApproach {
 
+    public HashMap MappedVals;
+
     public BruteForce(int[] inputList) {
         super(inputList);
+        this.MappedVals = new HashMap();
     }
 
     public void FindMode() {
@@ -40,6 +45,10 @@ class TransformNConquer extends BaseApproach {
     }
 
     public void FindMode() {
-        
+        mergeSort(this.List);
+    }
+
+    private void mergeSort(int[] listToSort) {
+
     }
 }
