f72ef55972b1f54b07451164518f194745ae09b6
Joe <joseph.mcadams@mu.edu>
2016-03-10 14:31:38 -0600

    Created basic outline for the class

---
+++ HeapSort.java
@@ -0,0 +1,27 @@
+public class HeapSort {
+
+    public int[] List;
+
+    public HeapSort(int[] passedList) {
+        this.List = passedList;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void Sort() {
+        createHeap();
+        heapSort();
+    }
+
+    // TODO: Rearrange elements to create the heap
+    private void createHeap() {
+
+    }
+
+    // TODO: At each swap, print out the contents of the array
+    private void heapSort() {
+
+    }
+}
