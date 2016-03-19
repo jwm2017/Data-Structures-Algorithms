bd9055f0bf2b21fcdace450a3c2b5401b4f9f928
Joe <joseph.mcadams@mu.edu>
2016-03-18 23:55:48 -0500

    Tentatively finished mergesort.py, need to test and see if the code can be shortened

--- MergeSort.py
+++ MergeSort.py
@@ -3,10 +3,6 @@ class MergeSort:
     def __init__(self, list):
         self.list = list
 
-    """
-    start and end indices optional so object can call
-    sort with no parameters initially: 'self.sort()'
-    """
     def sort(self):
         mergeSort(self.list)
 
@@ -20,33 +16,25 @@ class MergeSort:
             merge(rightArr)
             return merge(leftArr, rightArr)
 
-    def merge(lArr, rArr):
-        mergedArr
-
-
-        if start is None:
-            start = 0
-        if end is None:
-            end = len(self.list) - 1
-        mid = (end-start)/2
-
-        if (mid == 0): #start and end indices are the same
-            return self.list[mid]
-        if mid > 0:
-            sort(start, mid)
-            sort(mid + 1, end)
-
-        elif: #start and end indices are the same
-
-
-        indOfRightArr = mid + 1
-        indOfLeftArr = start
-        while (index)
-
-
+    def merge(leftArr, rightArr):
+        mergedArray
+        leftIndex, rightIndex
+        while (leftIndex < len(leftArr) && rightIndex < len(rightArr)):
+            if (leftArr[leftIndex] > rightArr[rightIndex]):
+                mergedArray.append(rightArr[rightIndex])
+                rightIndex++
+            else:
+                mergedArray.append(leftArr[leftIndex])
+                leftIndex++
+        while (leftIndex < len(leftArr)):
+            mergedArray.append(leftArr[leftIndex])
+            leftIndex++
+        while (rightIndex < len(rightArr)):
+            mergedArray.append(rightArr[rightIndex])
+            rightIndex++
+        return mergedArray
 
 def main():
-    """
     intList = MergeSort([4, 1, 5, 8, 9, 3, 3, 6, 7, 10, -5, -12])
     print('intList before sorting: \n' + intList + '\n\n')
     intList.sort()
@@ -57,13 +45,6 @@ def main():
     print('charList before sorting: \n' + charList + '\n\n')
     charList.sort()
     print('charList after sorting: \n' + charList + '\n\n')
-    """
-    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    left = data[:len(data)/2]
-    right = data[len(data)/2:]
-    print("Data: " + str(data))
-    print("Left: " + str(left))
-    print("Right: " + str(right))
 
 if __name__ == '__main__':
     main()
