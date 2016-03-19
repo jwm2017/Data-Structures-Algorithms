19b024d593e9f0cfffa9efcf94e68025fa31996c
Joe <joseph.mcadams@mu.edu>
2016-03-19 00:21:20 -0500

    I'm still transitioning from Java to Python...

--- MergeSort.py
+++ MergeSort.py
@@ -7,7 +7,7 @@ class MergeSort:
         mergeSort(self.list)
 
     def mergeSort(arr):
-        if len(arr) == 1
+        if (len(arr) == 1):
             return arr
         else:
             leftArr = arr[:len(arr)/2]
@@ -19,32 +19,32 @@ class MergeSort:
     def merge(leftArr, rightArr):
         mergedArray
         leftIndex, rightIndex
-        while (leftIndex < len(leftArr) && rightIndex < len(rightArr)):
+        while (leftIndex < len(leftArr) and rightIndex < len(rightArr)):
             if (leftArr[leftIndex] > rightArr[rightIndex]):
                 mergedArray.append(rightArr[rightIndex])
-                rightIndex++
+                rightIndex += 1
             else:
                 mergedArray.append(leftArr[leftIndex])
-                leftIndex++
+                leftIndex += 1
         while (leftIndex < len(leftArr)):
             mergedArray.append(leftArr[leftIndex])
-            leftIndex++
+            leftIndex += 1
         while (rightIndex < len(rightArr)):
             mergedArray.append(rightArr[rightIndex])
-            rightIndex++
+            rightIndex += 1
         return mergedArray
 
 def main():
     intList = MergeSort([4, 1, 5, 8, 9, 3, 3, 6, 7, 10, -5, -12])
-    print('intList before sorting: \n' + intList + '\n\n')
+    print('intList before sorting: \n' + str(intList) + '\n\n')
     intList.sort()
-    print('intList after sorting: \n' + intList + '\n\n')
+    print('intList after sorting: \n' + str(intList) + '\n\n')
     print(intList)
 
     charList = MergeSort(['a', 'z', 'c', 'b', 'f', 'e', 'y', 'p'])
-    print('charList before sorting: \n' + charList + '\n\n')
+    print('charList before sorting: \n' + str(charList) + '\n\n')
     charList.sort()
-    print('charList after sorting: \n' + charList + '\n\n')
+    print('charList after sorting: \n' + str(charList) + '\n\n')
 
 if __name__ == '__main__':
     main()
